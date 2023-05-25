efca313b100d27793b212261e1b76468e52cc7ec
Andiy2711 <125227629+Andiy2711@users.noreply.github.com>
2023-05-24 21:33:28 -0600

    HelloWorld

--- App.java
+++ App.java
@@ -3,12 +3,9 @@ package com.pp2;
 public class App {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wolrd <3");
 	
-		Thread t1 = new Thread (new Runner1());
-		Thread t2 = new Thread (new Runner2());
-		t1.start();
-		t2.start();
-
 	}
 
 }
