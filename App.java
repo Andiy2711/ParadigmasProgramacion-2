84af8627d99dfae15ea96d254c07e1ef64ddf7ea
Andiy2711 <125227629+Andiy2711@users.noreply.github.com>
2023-05-24 21:13:36 -0600

    Interface

--- App.java
+++ App.java
@@ -3,12 +3,10 @@ package com.pp2;
 public class App {
 
 	public static void main(String[] args) {
-		
-		Runner1 t1 = new Runner1();
-		Runner2 t2 = new Runner2();
-		
-		t1.start();
-		t2.start();
+		Dog dog = new Dog();
+		dog.move();
+		dog.eat();
+
 	}
 
 }
