69599428c2379da940b11a54723e794f1ead81e6
Andiy2711 <125227629+Andiy2711@users.noreply.github.com>
2023-05-24 21:15:14 -0600

    Locking

--- App.java
+++ App.java
@@ -2,11 +2,48 @@ package com.pp2;
 
 public class App {
 
-	public static void main(String[] args) {
-		Dog dog = new Dog();
-		dog.move();
-		dog.eat();
-
+	private static int counter = 0;
+		
+	public static synchronized void increment() {
+		++counter;
 	}
 
+	public static void process() {
+
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 100; ++i)
+					increment();
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 100; ++i)
+					increment();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+public static void main(String[] args) {
+
+	process();
+	System.out.println(counter);
+	
 }
+}
+
+	
