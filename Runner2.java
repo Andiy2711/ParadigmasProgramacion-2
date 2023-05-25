a482450394e4821a877639a7344b948627d8d1da
Andiy2711 <125227629+Andiy2711@users.noreply.github.com>
2023-05-24 21:20:14 -0600

    Multitreading2

--- Runner2.java
+++ Runner2.java
@@ -1,15 +1,9 @@
 package com.pp2;
 
-public class Runner2 extends Thread {
+public class Runner2 implements Runnable {
 	public void run() {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i<10;i++)
 			System.out.println("Runner 2: " + i);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 }
