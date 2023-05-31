29b902e85202856f9ff4052187c1910463e159d6
Andiy2711 <125227629+Andiy2711@users.noreply.github.com>
2023-05-31 07:44:19 -0600

    Locking_2

--- App.java
+++ App.java
@@ -1,30 +1,38 @@
 package com.pp2;
 
 public class App {
-
-	private static int counter = 0;
-		
-	public static synchronized void increment() {
-		++counter;
+	
+	private static int count1 = 0;
+	private static int count2 = 0;
+	
+	public synchronized static void add() {
+		count1++;
+	}
+	
+	public synchronized static void addAgain() {
+		count2++;
 	}
 
-	public static void process() {
+	public static void compute() {
+		for(int i=0;i<100;++i) {
+			add();
+			addAgain();
+		}
+	}
+	
+	public static void main(String[] args) {
 
 		Thread t1 = new Thread(new Runnable() {
-
 			@Override
 			public void run() {
-				for (int i = 0; i < 100; ++i)
-					increment();
+				compute();
 			}
 		});
-
+		
 		Thread t2 = new Thread(new Runnable() {
-
 			@Override
 			public void run() {
-				for (int i = 0; i < 100; ++i)
-					increment();
+				compute();
 			}
 		});
 		
@@ -37,13 +45,8 @@ public class App {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println("Count1="+count1+" - Count2="+count2);
 	}
-public static void main(String[] args) {
 
-	process();
-	System.out.println(counter);
-	
 }
-}
-
-	
